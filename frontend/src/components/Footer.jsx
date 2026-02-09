b4027f89ecbf5eef5124948e6f671c854932eb23
aabhishek866 <singhabhishek8525@gmail.com>
2026-02-09 13:56:28 +0530

    Update copyright name

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -34,7 +34,7 @@ const Footer = () => {
       </div>
       <div>
         <hr />
-        <p className='py-5 text-sm text-center'>Copyright 2025 @ JasminRoy - All Right Reserved.</p>
+        <p className='py-5 text-sm text-center'>Copyright 2025 @ AbhishekSingh - All Right Reserved.</p>
       </div>
     </div>
   )
